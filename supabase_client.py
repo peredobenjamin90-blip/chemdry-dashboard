b750cc0a0e1524f70ba3e8f2962b4bd0e20f0de9
Benjamin Peredo <benjaminperedo@Benjamins-MacBook-Air.local>
2026-05-22 12:15:21 -0600

    remove supabase temp

--- supabase_client.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-import streamlit as st
-
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
